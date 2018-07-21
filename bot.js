a1b759df75ce5ca0b593db0b3fddc565e7ca8969
Rcsve1 <41522704+Rcsve1@users.noreply.github.com>
2018-07-21 22:01:14 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "!";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
