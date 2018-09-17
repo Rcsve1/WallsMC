ac31342bf2eed8d2130ccb8db058ce602051247b
Rcsve1 <41522704+Rcsve1@users.noreply.github.com>
2018-09-17 19:30:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,22 +10,8 @@ client.on('ready', () => {
   member.addRole('name', "• New")
 });
 
-client.on('message', message => {
-  let log = message.guild.channels.find('name', "log") 
-  let act = message.guild.roles.find('name', "• Verified")
-  let user = message.mentions.members.first();
-  if(message.content.startsWith(prefix + "act")){
-    var embed = new Discord.RichEmbed() 
-    .setAuthor(message.author.username) 
-    .setThumbnail(user.avatarURL)
-    .addField('User Activated', ${user} get rank ${act})
-    .addField('By', <@${message.author.id}>)
-    .setTimestamp()
-    .setFooter("609 | copyrights")
-  log.send({embed})
-  message.channel.send({embed})
-  user.addRole(${act})
-  }
+client.on('guildMemberAdd', (member) => {
+member.addRole(member.guild.roles.find(●╠ 609 ╢ Member));
 });
 
 client.login('NDkxMjYyMjk0MDEwMTAxNzYy.DoFTag.Ag6KU_rUA_BVyFFFRVE5Rhq8zd4');
