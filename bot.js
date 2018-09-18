e6f0a15805d4c00b4bf385960a3a6f4ad27b8c24
Rcsve1 <41522704+Rcsve1@users.noreply.github.com>
2018-09-18 17:11:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '/'
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', member => {
+    member.guild.member(member.user).addRole(member.guild.roles.find(r => r.name === '●╠ 609 ╢ Member'));
+});
+
+         client.on('message', message => {
+            if (message.content.startsWith(prefix + "info")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+client.login('NDkxMjYyMjk0MDEwMTAxNzYy.DoKVbA.6zwfJBjYVv94gm-9CM0INC4p_Sg');
