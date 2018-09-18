d6cf3e021ac770f492ac4554b02996922362e8bb
Rcsve1 <41522704+Rcsve1@users.noreply.github.com>
2018-09-18 16:59:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '/'
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', member => {
+    member.guild.member(member.user).addRole(member.guild.roles.find(r => r.name === '●╠ 609 ╢ Member'));
+});
+
+
+
+
+
+
+
+client.login('NDkxMjYyMjk0MDEwMTAxNzYy.DoKVbA.6zwfJBjYVv94gm-9CM0INC4p_Sg');
