27c6a581cd9e530ee3678b3ca27946253bf89306
Rcsve1 <41522704+Rcsve1@users.noreply.github.com>
2018-09-18 16:29:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,16 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("●╠ 609 ╢ Member", "●╠ 609 ╢ Member");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
+
+
 client.login('NDkxMjYyMjk0MDEwMTAxNzYy.DoFTag.Ag6KU_rUA_BVyFFFRVE5Rhq8zd4');
